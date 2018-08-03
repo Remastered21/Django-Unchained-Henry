04a1555b0f8b0b036c7ebb5e8ccbb0717552b117
Henry K <mykim1891@hotmail.com>
2018-08-03 11:32:56 -0700

    removed redundancy

--- dj_unchained/settings.py
+++ dj_unchained/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
