8f727f49d798ad97eb699ed5fcaa8389923b70c2
Henry K <mykim1891@hotmail.com>
2018-08-08 16:36:22 -0700

    Created login page

---
+++ front-end-for-django/src/components/Login.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+export default class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      pwShowToggle: false,
+      isWrongCred: false
+    };
+  }
+
+  inputChangeHandler = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  submitHandler = e => {
+    const login = axios.post(
+      // 'address to in graphql',
+      this.state
+    );
+
+    e.preventDefault();
+
+    login
+      .then(response => {
+        localStorage.setItem("token", response.data.token);
+        this.props.history.push("/notes");
+      })
+      .catch(err => {
+        localStorage.removeItem("token");
+        this.setState({ isWrongCred: true });
+        this.setState({ password: "" });
+      });
+  };
+
+  pwToggler() {
+    const pwToggle = document.getElementById("pwInput");
+
+    if (pwToggle.type === "password") {
+      pwToggle.type = "text";
+    } else {
+      pwToggle.type = "password";
+    }
+  }
+
+  render() {
+    return (
+      <div className="Login-body">
+        <div className="Login-subBody">
+          <h1>Please enter your username and password</h1>
+          <form className="Login-form-body" onSubmit={this.submitHandler}>
+            <div className="IDpassword-body">
+              <p className="display">ID</p>
+              <input
+                className="input"
+                name="username"
+                value={this.state.username}
+                onChange={this.inputChangeHandler}
+                type="text"
+              />
+            </div>
+            <div>
+              <p className="display">Password</p>
+              <input
+                className="input"
+                name="password"
+                value={this.state.password}
+                onChange={this.inputChangeHandler}
+                type="password"
+                id="pwInput"
+              />
+            </div>
+            <div>
+              <input type="checkbox" onClick={this.pwToggler} />
+              Show Password
+            </div>
+            <br />
+            <div>
+              <button>Sign in</button>
+            </div>
+          </form>
+          <div>
+            <div>
+              {this.state.isWrongCred ? (
+                <h2 className="loginErrorMsg">
+                  Your username/password was incorrect.
+                </h2>
+              ) : null}
+            </div>
+
+            <br />
+            <div>
+              Don't have an assigned ID?
+              <br />
+              <br />
+              <Link to="/register">
+                <button>Request New ID</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
